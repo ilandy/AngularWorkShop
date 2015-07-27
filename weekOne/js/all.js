f498e04e7b73e6bc29d59b92bf49b7ebe813cf22
ilandy <ilandy@infbeyond.com>
2015-07-27 17:05:44 +0800

    week one github page

---
+++ weekOne/js/all.js
@@ -0,0 +1,17 @@
+var myApp = angular.module('myApp', ['wu.masonry']);
+    var api = 'https://api.pinterest.com/v3/pidgets/boards/ilandychiu/海報dm設計/pins/';
+
+    /* function 名稱小寫開頭會出錯 */
+    var MyController = function($scope, $http) {
+        $scope.pins = [];
+        $scope.select = true;
+        $http.jsonp(api + '?callback=JSON_CALLBACK').success(function(data) {
+            $scope.pins = data.data.pins;
+        }).error(function(data, status, headers, config) {
+            console.log('error');
+        });
+    };
+
+    /* 相依性注入：支援最小化 js 檔案 */
+    MyController.$inject = ['$scope', '$http'];
+    myApp.controller('MyController', MyController);
