08873c4276ce39a4b28ed2b442a9b0ec89fd55df
ilandy <ilandy@infbeyond.com>
2015-08-25 11:00:43 +0800

    簡化 判斷、取得連線數值 的function

--- weekFour/js/all.js
+++ weekFour/js/all.js
@@ -1,8 +1,3 @@
-/**
-*  Module
-*
-* Description
-*/
 var app = angular.module('myApp', []);
 var OXController = function ($scope){
 	$scope.whoSelect = 0;
@@ -17,64 +12,27 @@ var OXController = function ($scope){
 		if(reload == true) {
 			window.location.reload();
 		}
-	}
-	$scope.checkWin = function(row,val)
-	{
-		var flag = row.some(function (value, index, array) {return value == val ? true : false;});
-		return flag;
-	}
-
-	$scope.getWinLine = function(){
-		var row=[] ,col=[] ,slash=[] ,all=0 ;
-		for(i=0; i<$scope.items.length; i++) {all=parseInt(all+$scope.items[i])}
-		
-		// 判斷row
-		for(j=0; j<3; j++) 
-			{			
-			row[j] = parseInt($scope.items[j*3]+$scope.items[j*3+1]+$scope.items[j*3+2]);
-			if($scope.checkWin(row,0) == true)
-			{
+	} // -> 重新來一次
+	$scope.checkWin = function(row){
+		if(row.some(function (value, index, array) {return value == 0 ? true : false;}) == true){
 				$scope.confirm('玩家1獲勝!再來一場吧？');
-				break;
-			}else if($scope.checkWin(row,3) == true)
-				{
-					$scope.confirm('玩家2獲勝!再來一場吧？');
-					break;
-				}
+			}else if(row.some(function (value, index, array) {return value == 3 ? true : false;}) == true){
+				$scope.confirm('玩家2獲勝!再來一場吧？');
 			}
-		// 判斷col
-		for(j=0; j<3; j++) 
-		{			
-			col[j] = parseInt($scope.items[j]+$scope.items[j+3]+$scope.items[j+6]);
-			if($scope.checkWin(col,0) == true)
-			{
-				$scope.confirm('玩家1獲勝!再來一場吧？');
-				break;
-			}else if($scope.checkWin(col,3) == true)
-				{
-					$scope.confirm('玩家2獲勝!再來一場吧？');
-					break;
-				}
-		}
-		// 判斷slash
-		for(j=0; j<2; j++) 
-		{			
-			slash[j] = parseInt($scope.items[4+(-4+j*2)]+$scope.items[4]+$scope.items[4+(4-j*2)]);
-			if($scope.checkWin(slash,0) == true)
-			{
-				$scope.confirm('玩家1獲勝!再來一場吧？');
-				break;
-			}else if($scope.checkWin(slash,3) == true)
-				{
-					$scope.confirm('玩家2獲勝!再來一場吧？');
-					break;
-				}
+	} // -> 確認比對取得數值是否為 true 
+	$scope.getWinLine = function(){
+		var row=[] ,col=[] ,slash=[], whoLined=[row,col,slash] ,all=0 ;
+		for(i=0; i<$scope.items.length; i++) {all=all+$scope.items[i]}//判斷全滿
+		for(j=0; j<3; j++){	
+			row[j] = parseInt($scope.items[j*3]+$scope.items[j*3+1]+$scope.items[j*3+2]); // 判斷row
+			col[j] = parseInt($scope.items[j]+$scope.items[j+3]+$scope.items[j+6]); // 判斷col
+			if(j<2){ slash[j] = parseInt($scope.items[4+(-4+j*2)]+$scope.items[4]+$scope.items[4+(4-j*2)]);}// 判斷slash
+			$scope.checkWin(whoLined[j]);
 		}
 		if(all==4){
 			$scope.confirm('不分勝負!再來一場吧？');
 		}
-	}
-	
+	}// -> 取得加總 行、列、斜線 全部連線值並判斷
 	$scope.pinSelected = function(index){
 		if( $scope.items[index] != 4 ){
 			alert('你這個學人精！有點創意好嗎');
@@ -82,7 +40,6 @@ var OXController = function ($scope){
 			$scope.pinIt(index);
 			$scope.getWinLine();
 		}
-		
 	}
 }
 app.controller('OXController', OXController)
